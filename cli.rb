a47399fb3d2f7027bc668e439d558b52ab600dc8
airportyh <airportyh@gmail.com>
2013-05-12 22:10:36 -0400

    Spiked on simple searching.

--- cli.rb
+++ cli.rb
@@ -1,8 +1,27 @@
 #! /usr/bin/env ruby
 
-require 'findcode'
+require './findcode'
+require 'colorize'
 
 filename = ARGV[0]
+query = ARGV[1..ARGV.size]
+puts "Query: #{query}"
 puts "Reading #{filename}"
 content = File.read(filename)
-puts FindCode.new.tokenize(content)
+lines = content.split("\n")
+find_code = FindCode.new
+lines.each_with_index do |line, index|
+  matches = find_code.find_match(query[0], line)
+  if matches.size > 0 then
+    display_line = ''
+    idx = 0
+    matches.each do |match|
+      display_line += line[idx..match.pos - 1] +
+        line[match.pos..match.pos + match.text.size - 1].green
+      idx = match.pos + match.text.size
+    end
+    display_line += line[idx..line.size]
+    puts "#{index}: #{display_line}"
+  end
+  
+end
